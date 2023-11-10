4d0f613ded129011bbc661f64073b0eb6039dc31
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-10 09:57:55 -0300

    add a msg "fim de jogo" e modificando detalhes

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,15 +1,16 @@
-print("*********************************")
+print("*"*33)
 print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************\n")
+print("*"*33)
 
 numero_secreto = 42
 
 chute = int(input("Digite um número: "))
 
-print("Você digitou", chute)
+print("\nVocê digitou", chute)
 
 if numero_secreto == chute:
     print("\nParabéns, você acertou!")
 else:
     print("\nQue pena, você errou.")
 
+print("Fim do jogo!")
