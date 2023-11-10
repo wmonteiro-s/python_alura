fe599bbb036ef39832d2d6167fcbd0fbe4d6d39a
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-10 10:15:58 -0300

    melhorando e simplicando código

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,9 +8,16 @@ chute = int(input("Digite um número: "))
 
 print("\nVocê digitou", chute)
 
-if numero_secreto == chute:
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if acertou:
     print("\nParabéns, você acertou!")
 else:
-    print("\nQue pena, você errou.")
+    if maior:
+        print("\nQue pena, você errou. O seu chute foi MAIOR que o número secreto")
+    elif menor:
+        print("\nQue pena, você errou. O seu chute foi MENOR que o número secreto")
 
 print("Fim do jogo!")
