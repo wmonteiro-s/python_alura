d43140ed8e74566c9f612bc9b3a41fb330187515
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-26 17:12:49 -0300

    criando a função "jogar"

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,63 +1,64 @@
 import random
 
-print("*"*33)
-print("Bem vindo ao jogo de Adivinhação!")
-print("*"*33, "\n")
+def jogar():
+    print("*"*33)
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*"*33, "\n")
 
-numero_secreto = random.randrange(1,101)
-tentativas = 1
-max_tentativas = 0
-pontos = 1000
-print(numero_secreto)
+    numero_secreto = random.randrange(1,101)
+    tentativas = 1
+    max_tentativas = 0
+    pontos = 1000
+    print(numero_secreto)
 
-print("\nEscolha o nível de dificuldade\n(1)Fácil | (2)Médio | (3)Difícil")
+    print("\nEscolha o nível de dificuldade\n(1)Fácil | (2)Médio | (3)Difícil")
 
-while True:
-    nivel = int(input("\nSelecione um dos níveis indicados: "))
+    while True:
+        nivel = int(input("\nSelecione um dos níveis indicados: "))
 
-    if nivel == 1:
-        print("<-Nível Fácil->\n")
-        max_tentativas = 20
-        break
-    elif nivel == 2:
-        print("<-Nível Médio->\n")
-        max_tentativas = 10
-        break
-    elif nivel == 3:
-        print("<-Nível Difícil->\n")
-        max_tentativas = 5
-        break
-    else:
-        print("***Este não é um nível selecionável***")
-    
-for tentativas in range(tentativas, max_tentativas + 1):
-    print(f'Tentativa {tentativas} de {max_tentativas}')
-    chute = int(input("Digite um número entre 1 e 100: "))
-    print(f'Você digitou {chute}')
+        if nivel == 1:
+            print("<-Nível Fácil->\n")
+            max_tentativas = 20
+            break
+        elif nivel == 2:
+            print("<-Nível Médio->\n")
+            max_tentativas = 10
+            break
+        elif nivel == 3:
+            print("<-Nível Difícil->\n")
+            max_tentativas = 5
+            break
+        else:
+            print("***Este não é um nível selecionável***")
+        
+    for tentativas in range(tentativas, max_tentativas + 1):
+        print(f'Tentativa {tentativas} de {max_tentativas}')
+        chute = int(input("Digite um número entre 1 e 100: "))
+        print(f'Você digitou {chute}')
 
-    if chute < 1 or chute > 100:
-        print("O número digitado deve estar entre 1 e 100\n")
-        continue
+        if chute < 1 or chute > 100:
+            print("O número digitado deve estar entre 1 e 100\n")
+            continue
 
-    acertou = numero_secreto == chute
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+        acertou = numero_secreto == chute
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
-    if acertou:
-        print(f'Parabéns, você acertou e fez {pontos} pontos!\n')
-        break
-    elif maior:
-        print("Que pena, você errou. O seu chute foi MAIOR que o número secreto\n")
-        if tentativas == max_tentativas:
-            print(f'Você esgotou suas tentativas, o número secreto era {numero_secreto}')
-    elif menor:
-        print("Que pena, você errou. O seu chute foi MENOR que o número secreto\n")
-        if tentativas == max_tentativas:
-            print(f'Você esgotou suas tentativas, o número secreto era {numero_secreto}')      
-    else:
-        print("Não é válido\n")
+        if acertou:
+            print(f'Parabéns, você acertou e fez {pontos} pontos!\n')
+            break
+        elif maior:
+            print("Que pena, você errou. O seu chute foi MAIOR que o número secreto\n")
+            if tentativas == max_tentativas:
+                print(f'Você esgotou suas tentativas, o número secreto era {numero_secreto}')
+        elif menor:
+            print("Que pena, você errou. O seu chute foi MENOR que o número secreto\n")
+            if tentativas == max_tentativas:
+                print(f'Você esgotou suas tentativas, o número secreto era {numero_secreto}')      
+        else:
+            print("Não é válido\n")
 
-    pontos_perdidos = abs(numero_secreto - chute)
-    pontos = pontos - pontos_perdidos
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
-print("Fim do jogo!")
+    print("Fim do jogo!")
---
+++ jogos/forca.py
@@ -0,0 +1,4 @@
+def jogar():
+    print("*"*27)
+    print("Bem vindo ao jogo da Forca!")
+    print("*"*27, "\n")
