a2cbf47b25ac481cd44f252a7143ad5a8861d963
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-28 00:25:44 -0300

    Otimizando arquivo utilizando "while"

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -1,17 +1,24 @@
 import forca
 import adivinhacao
 
-print("*********************************")
-print("*******Escolha o seu jogo!*******")
-print("*********************************")
+while True:
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca | (2) Adivinhação")
 
-jogo = int(input("Qual jogo? "))
+    jogo = int(input("Qual jogo? "))
 
-if (jogo == 1):
-    print("Jogando forca")
-    forca.jogar()
-elif (jogo == 2):
-    print("Jogando adivinhação")
-    adivinhacao.jogar()
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+    saida = input("Você deseja continuar jogando? [S]Sim [N]Não: ").upper()
+    if saida.strip() == saida.strip("S") or saida.strip() == saida.strip("SIM"):
+        break
+    elif saida.strip() == saida.strip("N") or saida.strip() == saida.strip("NÃO") or saida.strip() == saida.strip("NAO"):
+        continue
